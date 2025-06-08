40f9776576f791b6d4b561e1931c6b69619407ad
dodi121 <faldioardiarahman1@gmail.com>
2025-06-09 06:15:44 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
-global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.creator = 'Bangellxs'// yourname
+global.MONGO_DB_URI = "mongodb+srv://BangEllxs:BangEllxs123@bangellxs.a1izsxx.mongodb.net/?retryWrites=true&w=majority&appName=BangEllxs" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "ellxsxzaja" //isi apa aja bebas
+global.your_email = "faldioardiarahman10@gmail.com" //email
+global.email_password = "rschaefkxifwnnub" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
